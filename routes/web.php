843095da198cca680bb6208dcaca65f404d6a927
Princess Garde <princessmaygarde@gmail.com>
2021-10-03 16:31:16 +0800

    index route

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 
-Route::group(['prefix' => 'admin'], function () {
+Route::group(['prefix' => '/'], function () {
     Voyager::routes();
 });
