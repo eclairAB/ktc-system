52ec8d95fd8c4a1ab314c84b38271c1244ad80c8
Princess May Garde <38845902+princessgarde@users.noreply.github.com>
2021-10-16 18:39:54 +0800

    Princess (#21)
    * index route
    
    * try
    
    * fix conflict
    
    * login design
    
    * dynamic login background image
    
    * empty dashboard
    
    * update login ui
    
    * change login button color
    
    * adjust login logo
    
    * navbar logo and title
    
    * form initial
    
    * v-models initial
    
    * form updates
    
    * default form ui
    
    * remove unnecessary css
    
    * ui updates
    
    * typo

--- resources/views/vendor/voyager/container-releasings/edit-add.blade.php
+++ resources/views/vendor/voyager/container-releasings/edit-add.blade.php
@@ -40,11 +40,11 @@
                               </div>
                               <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 form-group" style="padding-right: 5px; padding-left: 5px;">
                                 <input type="text" name="seac_no" id="seac_no" v-model="form.seac_no" class="form-control" style="height: 37px;">
-                                <label for="seac_no" class="form-control-placeholder"> Seac No</label>
+                                <label for="seac_no" class="form-control-placeholder"> Seal No</label>
                               </div>
                               <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 form-group" style="padding-right: 5px; padding-left: 5px;">
                                 <input type="text" name="conglone" id="conglone" v-model="form.conglone" class="form-control" style="height: 37px;">
-                                <label for="conglone" class="form-control-placeholder"> Conglone</label>
+                                <label for="conglone" class="form-control-placeholder"> Consignee</label>
                               </div>
                               <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 form-group" style="padding-right: 5px; padding-left: 5px;">
                                 <input type="text" name="hauler" id="hauler" v-model="form.hauler" class="form-control" style="height: 37px;">
