8cd40fcc0f3347376369cf88787a1d63c687a068
rhinlog <raymunddh@gmail.com>
2021-10-17 20:25:03 +0800

    signature

--- resources/views/vendor/voyager/container-receivings/edit-add.blade.php
+++ resources/views/vendor/voyager/container-receivings/edit-add.blade.php
@@ -9,6 +9,13 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://unpkg.com/vue-select@latest/dist/vue-select.css">
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/main.css') }}">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" integrity="sha512-aOG0c6nPNzGk+5zjwyJaoRUgCdOrfSDhmMID2u4+OIslr0GjpLKo7Xm0Ao3xmpM4T8AmIouRkqwj1nrdVsLKEQ==" crossorigin="anonymous" />     
+    <link rel="stylesheet" type="text/css" href="http://keith-wood.name/css/jquery.signature.css">
+    <style>
+        .kbw-signature { width: 100%; height: 200px;}
+        #sig canvas{ width: 100% !important; height: auto;}
+    </style>  
 @stop
 
 @section('page_title', __('voyager::generic.'.($edit ? 'edit' : 'add')).' '.$dataType->getTranslatedAttribute('display_name_singular'))
@@ -204,6 +211,30 @@
                           <div class="col-xs-12" style="border-bottom: 1px solid #e4eaec; padding-bottom: 10px; margin-bottom: 10px;">
                             <div style="font-weight: 700; font-size: 15px; color: black;">Signature</div>
                           </div>
+                          <div class="col-md-6 offset-md-3 mt-5">
+                            <div class="card">
+                              <div class="card-body">
+                                  @if ($message = Session::get('success'))
+                                      <div class="alert alert-success  alert-dismissible">
+                                          <button type="button" class="close" data-dismiss="alert">×</button>  
+                                          <strong>{{ $message }}</strong>
+                                      </div>
+                                  @endif
+                                  <form>
+                                      @csrf
+                                      <div class="col-md-12">
+                                          <label class="" for="">Draw Signature:</label>
+                                          <br/>
+                                          <div id="sig_receiving"></div>
+                                          <br><br>
+                                          <button id="clear" class="btn btn-danger">Clear Signature</button>
+                                          <button class="btn btn-success">Save</button>
+                                          <textarea id="signature_receiving" name="signature" style="display: none"></textarea>
+                                      </div>
+                                  </form>
+                              </div>
+                            </div>
+                          </div>
                         </div>
                       </div>
                     </div>
@@ -238,6 +269,17 @@
 @stop
 
 @section('javascript')
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script> 
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="http://keith-wood.name/js/jquery.signature.js"></script>
+    <script type="text/javascript">
+        var sig_receiving = $('#sig_receiving').signature({syncField: '#signature_receiving', syncFormat: 'PNG'});
+        $('#clear').click(function(e) {
+            e.preventDefault();
+            sig_receiving.signature('clear');
+            $("#signature_receiving").val('');
+        });
+    </script>
     <script>
         var params = {};
         var $file;
