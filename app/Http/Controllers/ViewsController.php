32c829c55a1716eaf17f4916cebf33e95d73cb93
eclair <alanbenedictgolpeo@gmail.com>
2021-10-31 00:31:12 +0800

    updates on dashboard and routing

--- app/Http/Controllers/ViewsController.php
+++ app/Http/Controllers/ViewsController.php
@@ -12,8 +12,13 @@ class ViewsController extends \TCG\Voyager\Http\Controllers\VoyagerBaseControlle
         return redirect()->intended('/admin/' . Auth::user()->role->name);
     }
 
-    public function roleView($role)
+    public function roleView($url_role)
     {
+        $role = Auth::user()->role->name;
+
+        if($url_role != $role) {
+            return $this->roleRedirect();
+        }
         return view('vendor.voyager.role-views.'.$role, ['name' => $role]);
     }
 }
