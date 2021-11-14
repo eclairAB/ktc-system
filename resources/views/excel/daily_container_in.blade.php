da97ea21c9e2d8aa192f44dbf1eab405de4cb270
rhinlog <raymunddh@gmail.com>
2021-11-14 22:10:42 +0800

    updates

---
+++ resources/views/excel/daily_container_in.blade.php
@@ -0,0 +1,33 @@
+<table>
+    
+        <tr>
+          <th style="font-weight:bold;">EIR No.</th> 
+          <th style="font-weight:bold;">Container No.</th> 
+          <th style="font-weight:bold;">Size/Type</th>
+          <th style="font-weight:bold;">Date IN</th>
+          <th style="font-weight:bold;">Shipping Line</th>
+          <th style="font-weight:bold;">Truckers</th>
+          <th style="font-weight:bold;">Plate No.</th>
+          <th style="font-weight:bold;">Inspected By</th>
+          <th style="font-weight:bold;">Class</th>
+          <th style="font-weight:bold;">Manufactured Date</th>
+          <th style="font-weight:bold;">Status</th>
+          <th style="font-weight:bold;">Remarks</th>
+        </tr>
+        @foreach($data as $key => $item)
+        <tr>
+          <td>{{ $item->id }}</td>
+          <td>{{ $item->container_no }}</td>
+          <td>{{ $item->sizeType->code }} - {{ $item->sizeType->name }}</td>
+          <td>{{ Carbon\Carbon::parse($item->created_at)->format('Y-m-d') }}</td>
+          <td>{{ $item->client->code_name }}</td>
+          <td>{{ $item->hauler }}</td>
+          <td>{{ $item->plate_no }}</td>
+          <td>{{ $item->inspector->name }}</td>
+          <td>{{ $item->containerClass->class_code }}</td>
+          <td>{{ Carbon\Carbon::parse($item->manufactured_date)->format('Y-m-d') }}</td>
+          <td>Received</td>
+          <td>{{ $item->remarks }}</td>
+        </tr>
+      @endforeach
+</table>
