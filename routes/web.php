c454df427aba1d69e140aba5b31334fc61cac513
eclair <alanbenedictgolpeo@gmail.com>
2021-12-20 13:22:27 +0800

    updates for eir and *

--- routes/web.php
+++ routes/web.php
@@ -60,7 +60,6 @@ Route::group(['prefix' => 'admin'], function () {
 
     Route::get('/get/releasing/byId/{id}',[QueriesController::class,"getReleasingById"]);
     Route::get('/get/receiving/byId/{id}',[QueriesController::class,"getReceivingById"]);
-    Route::post('/create/eir',[UpdateController::class,"createEir"]);
 
     Route::get('/get/sizeType/byId/{id}',[QueriesController::class,"getSizeTypeById"]);
     Route::get('/get/client/byId/{id}',[QueriesController::class,"getClientById"]);
