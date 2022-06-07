7d4a5aa60d87b2e92c725ef71a45a18d3bad2099
nreactor <36671402+eclairAB@users.noreply.github.com>
2022-06-07 23:24:46 +0800

    Benlan (#366)
    * hatpeks again
    
    * added gitignore file
    
    * updates
    
    * unions
    
    * yawa na inquiry hgahahaha
    
    * display updates for inquiry (unfinished)
    
    * apdets
    
    * saving rework
    
    * download initial
    
    * displays update
    
    * hotfix
    
    * hotfix
    
    * staff fix
    
    * signature cleanup
    
    * pagination search
    
    * update
    
    * updates for eir and *
    
    * hatpeks
    
    * coldfix
    
    * revisions `Dec 22` fix attempt #1
    
    * browse blade update
    
    * reroute revert on generic browse
    
    * initial tabbing feature
    
    * dropdown update day 2
    
    * tabbing update completion
    
    * reworks on tabbing
    
    * fix on tabbing feature
    
    * hotfix!!!
    
    * saving update
    
    * hotfix unfinished
    
    * fixed tabbing issue
    
    * hightlight on autocomplete select
    
    * autocomplete tabbing finished
    
    * revision fixes
    
    * update peks
    
    * dashboard update
    
    * made container photo not required
    
    * mali haha
    
    * pekses
    
    * peks on update without container image
    
    * validation on aging
    
    * conflict fix
    
    * fix
    
    * removed extra class
    
    * cdn updates
    
    * cursor rework and reroute fix
    
    * new Filter UI
    
    * show error validation
    
    * remove unnecessary data binding
    
    * v-select version update
    
    * removed chuchu
    
    * updateee
    
    * added tabbing
    
    * tabindexes
    
    * hotfix
    
    * nowrap update
    
    * removing division on modules
    
    * specing

--- resources/views/vendor/voyager/container-actions/out.blade.php
+++ resources/views/vendor/voyager/container-actions/out.blade.php
@@ -1,7 +1,7 @@
 <div id="containerOut">
 
   <div class="panel panel-default" style="margin-top: 15px;">
-    <div class="panel-body">
+    <div class="panel-body" style="padding-bottom: 0;">
       <div class="row">
         <div class="col-xs-12" style="margin-bottom: 0;">
           <div class="row" style="margin: 0; width: 100%;">
@@ -116,7 +116,7 @@
       </div>
     </div>  
 
-    <div class="panel-body">
+    <div class="panel-body" style="padding-top: 0;">
   		<div style="color: black; font-weight: bold; text-align: center; margin-bottom: 10px;">
           <!-- <img src = "{{ asset('/images/kudos.png') }}" width="150px" /><br> -->
           <span>Container Daily Out Report</span>
